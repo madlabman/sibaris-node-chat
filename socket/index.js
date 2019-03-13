16d7dafe51cceae60d1f66b2819e5f6b1ab7e6e3
chtotonetak <madlabman@mail.ru>
2019-03-13 10:09:38 +0100

    Change JWT auth

--- socket/index.js
+++ socket/index.js
@@ -1,23 +1,39 @@
 'use strict';
 
 const Server = require('socket.io');
+const socketIoJwt = require('socketio-jwt');
 const events = require('./eventsTypes');
 const handlers = require('./handlers');
 
-const CHAT_SERVER_PATH = (() => {
+const config = (() => {
     if (process.env.NODE_ENV === 'production') {
-        return process.env.chatServerPath;
+        return {
+            CHAT_SERVER_PATH: process.env.chatServerPath,
+            JWT_AUTH_SECRET: process.env.jwtAuthSecret
+        }
     } else {
-        return '/chat';
+        return {
+            CHAT_SERVER_PATH: '/chat',
+            JWT_AUTH_SECRET: 'some_secret_key'
+        }
     }
 })(); // TODO: Возможно, слить все в один конфиг
 
 const ioServer = server => {
     const io = new Server(server, {
-        path: CHAT_SERVER_PATH
+        path: config.CHAT_SERVER_PATH
     });
+    // Привязвка аутентификации по токену
+    io.use(socketIoJwt.authorize({
+        secret: config.JWT_AUTH_SECRET,
+        handshake: true
+    }));
     // Привязка событий
     io.on('connection', socket => {
+        console.log({
+            'socketId': socket.id,
+            'login': socket.decoded_token.data.login
+        });
         socket.on(events.GET_USERS, handlers.getUsersHandler)
     });
     return io;
